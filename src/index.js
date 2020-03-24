67ba4ead62208237f7cbc659ef9d2ff4f4fe9c93
Lucas Gustavo <lucas.gustavoc@live.com>
2020-03-24 18:45:25 -0300

    Setup inicial

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<p>Here we are</p>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
+    }
+}
